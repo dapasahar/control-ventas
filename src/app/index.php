9d2e80d40917f48a17c3e46024e1488127186135
Daniel Palacio <dapasa@har.upv.es>
2025-03-25 11:44:37 +0100

    fix: conexión a BBDD

--- src/app/index.php
+++ src/app/index.php
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
 }
 // endregion
 
-$sql = "SELECT * FROM `usuarios` WHERE `nombre_usuario` = '".$_POST['usuario']."' AND `password` = '".$_POST['password']."'";
+$sql = "SELECT * FROM `usuarios` WHERE `email` = '".$_POST['usuario']."' AND `password` = '".$_POST['password']."'";
 $result = $conn->query($sql);
 
 if ($result->num_rows == 0) {
@@ -26,6 +26,25 @@ if ($result->num_rows == 0) {
 }
 
 ?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Control de Ventas</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+    <script src="../js/code.js" defer></script>
+</head>
+<body>
+
+<header>
+    <img src="https://placehold.co/100x70" alt="Logotipo">
+</header>
 
 <h1>Bienvenido <?php echo $_POST['usuario']; ?></h1>
+
+</body>
+</html>
 
