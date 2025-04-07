7ec8e94ad3f9fe40ba8af1dac26a4b8e90cf3d9e
Daniel Palacio <dapasa@har.upv.es>
2025-04-07 10:54:18 +0200

    feat: menú vendedor/gestor

--- src/app/ventas.php
+++ src/app/ventas.php
@@ -1,1 +1,20 @@
 <?php
+
+?>
+  <!doctype html>
+  <html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Control de Ventas</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+    <script src="../js/code.js" defer></script>
+  </head>
+  <body>
+
+<?php include 'includes/menu.inc'; ?>
+  
+  </body>
+</html>
